bef7273ee59f0a921e2c3404b842d22330f25baf
Felipe Melo <felipesilvamelo28@gmail.com>
2022-03-14 20:01:29 -0300

    criando data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into book values (1, 'Livro Teste 1', '2022', 1.0, 'Felipe Melo');
+insert into book values (2, 'Livro Teste 2', '2022', 1.1, 'Felipe Melo');
+insert into book values (3, 'Livro Teste 3', '2022', 1.2, 'Felipe Melo');
