32708b232f430143d214cc9847527e5dfdb654f2
Felipe Melo <felipesilvamelo28@gmail.com>
2022-03-14 18:34:19 -0300

    commit inicial

---
+++ src/main/kotlin/br/com/blacklab/labspringbootkotlinbasico/LabSpringBootKotlinBasicoApplication.kt
@@ -0,0 +1,11 @@
+package br.com.blacklab.labspringbootkotlinbasico
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LabSpringBootKotlinBasicoApplication
+
+fun main(args: Array<String>) {
+	runApplication<LabSpringBootKotlinBasicoApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/blacklab/labspringbootkotlinbasico/LabSpringBootKotlinBasicoApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.blacklab.labspringbootkotlinbasico
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LabSpringBootKotlinBasicoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
